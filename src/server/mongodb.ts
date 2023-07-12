f929884aabf9afb406d500f9b25821bd6939b445
Bazhan Nagnybida <bazhantt@gmail.com>
2023-07-12 09:38:36 -0400

    don't use dotenv if production

--- src/server/mongodb.ts
+++ src/server/mongodb.ts
@@ -1,10 +1,9 @@
 import { MongoClient } from 'mongodb';
 import { join } from "path";
-import { config } from 'dotenv';
 
 
 console.log(process.env["NODE_ENV"]);
-if (process.env["NODE_ENV"] !== "production") config({ path: join(process.cwd(), "src/server", ".env") });
+if (process.env["NODE_ENV"] !== "production") require("dotenv").config({ path: join(process.cwd(), "src/server", ".env") });
 
 const client = new MongoClient(process.env["MONGO_URL"] as string);
 
